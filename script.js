ac10c0c0e288c0c1db2fef3e241b1485a20ebbbb
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-11-13 03:28:50 +0000

    .

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 let sendBtn = document.getElementsByClassName('send_btn')[0];
 let input = document.getElementsByClassName('input')[0];
 let chat = document.getElementsByClassName('chat')[0];
-let message = document.getElementsByClassName('message')[0];
 
 function findInputText() {
-    let inputText; // Declare inputText here
-
     sendBtn.addEventListener('click', function () {
-        inputText = input.value;
-        input.value = '';
-        cloneMessages();
+        let inputText = input.value;
+        if (inputText.trim() !== '') {
+            input.value = '';
+            cloneMessages(inputText);
+        }
     });
 
     input.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
-            inputText = input.value;
-            input.value = '';
-            cloneMessages();
+            let inputText = input.value;
+            if (inputText.trim() !== '') {
+                input.value = '';
+                cloneMessages(inputText);
+            }
         }
     });
 }
 
-findInputText();
-
-function cloneMessages() {
-    chat.innerHTML += `<div class="message">
+function cloneMessages(text) {
+    let messageDiv = document.createElement('div');
+    messageDiv.classList.add('message');
+    messageDiv.innerHTML = `
         <img src="question_mark.png" alt="" class="icon">
-        <div class="message_text">${inputText}</div>
-    </div>`;
+        <div class="message_text">${text}</div>
+    `;
+    chat.appendChild(messageDiv);
 }
+
+findInputText();
